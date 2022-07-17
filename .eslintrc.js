590d773d8a159f9f6cd7fafb100ceed8e3fc6c39
Joydip Roy <joydipand@gmail.com>
2022-07-17 18:27:39 +0000

    feat: ci/cd UI testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
